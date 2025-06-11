07c467d47e0a6804eda68d03ef6615cce8074a9a
sntosgstv <gustavo.santos1506@escola.pr.gov.br>
2025-06-11 01:20:21 +0000

    perguntas kiss

--- perguntas.js
+++ perguntas.js
@@ -1,71 +1,71 @@
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Onde ocorreu o incêndio da boate Kiss?',
     'Em santa maria, rio grande do sul, Brasil'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Quando aconteceu o incêndio?',
     'Na madrugada de 27 de janeiro de 2013'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'quantas pessoas morreram no incidente?',
     '242 Pessoas morreram'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Qual foi a principal causa do incêndio?',
     'Um sinalizador de fumaça acionado pela banda no palco.'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Por que o incêndio se espalhou tão rápido?',
     'Devido ao material inflamável no teto e à falta de saídas de emergência adequadas.'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'A boate tinha alvará de funcionamento?',
     'Sim, mas irregular e com falhas nas inspeções.'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Quais foram os principais responsáveis condenados?',
     'Os donos da boate, os integrantes da banda e dois fiscais da prefeitura.'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Qual foi a principal falha de segurança da boate?',
     'A falta de saídas de emergência e extintores em condições.'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Qual era o nome da banda que se apresentava naquela noite?',
     'Banda Gurizada Fandangueira.'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Qual foi a reação do governo após o incidente?',
     'Foram criadas novas normas de prevenção a incêndios e homenagens às vítimas.'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Como o incêndio afetou a legislação brasileira?',
     'Leis mais rígidas de segurança em casas noturnas foram criadas.'
 )
 
 criaCartao(
-    'incêndio da boate kiss',
+    'boate kiss',
     'Houve algum sobrevivente?',
     'Sim, cerca de 600 pessoas sobreviveram, muitas com sequelas graves.'
 )
